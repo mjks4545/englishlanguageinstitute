91d7a1ff7ec59ceb9ecd4c5e3de3b461303a7e95
SE-Bilal <hazratbilal.riu@gmail.com>
2016-04-27 10:41:21 +0500

    payment under process [4/27/2016 10:40 am]
    payment under process [4/27/2016 10:40 am]

--- application/views/admin/student/student_details.php
+++ application/views/admin/student/student_details.php
@@ -6,7 +6,12 @@
             <li class="active">Student Details</li>
         </ol>
     </div><!--/.row-->
-    
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Student Details</h1>
+        </div>
+    </div><!--/.row-->
+
 
 
     <div class="row">
